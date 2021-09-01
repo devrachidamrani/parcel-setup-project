198f0974db2f8ff138a61645956708e81a8d2db7
devrachidamrani <rachid3alaoui@gmail.com>
2021-09-01 17:41:24 +0100

    show modern js transpiling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 console.log('Is working !!')
 
+const obj = {
+  a: 'apple',
+  i: 'ice',
+  c: 'cheese',
+  d: {
+    cd: 'cheese danish',
+  },
+}
+
+const newObj = { ...obj }
+
+console.log(obj?.f?.fa)
+
 if (module.hot) {
   module.hot.accept()
 }
