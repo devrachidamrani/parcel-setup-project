f5d4daa59981d33af3b05e964a48b236f46cda9b
devrachidamrani <rachid3alaoui@gmail.com>
2021-09-02 14:17:04 +0100

    add styled components

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -1,16 +1,31 @@
 import React, { useState } from 'react'
 import me from '../images/me.jpeg'
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+place-items: center;
+background-color: midnightblue;
+color: #f1f1f1;
+padding: 1rem;
+font-family: Ubuntu;
+
+h2 {
+  font-size: 50px;
+  color: darkorange;
+}
+`
 
 const App = () => {
-
   const [state,setState] = useState(0)
   return (
-    <div>
+    <Container>
       <h2>Hey ! Hello React! </h2>
       <p>State : {state}</p>
-
-      <img src={me} alt="me" />
-    </div>
+      <img src={me} alt="me" width="300" />
+    </Container>
   )
 }
 
