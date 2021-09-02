f264bf8df793fd3aa837cba0a4399d3c4ae5ce20
devrachidamrani <rachid3alaoui@gmail.com>
2021-09-02 12:30:27 +0100

    add postcss, browserslist, and css modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import './scss/default.scss'
+import './styles/default.scss'
+import styles from './styles/module-styles.css'
 
 console.log('Is working !!')
 
@@ -14,6 +15,10 @@ const obj = {
 const newObj = { ...obj }
 
 console.log(obj?.f?.fa)
+
+const h2 = document.querySelector('h2')
+
+h2.classList.add(styles.heading)
 
 if (module.hot) {
   module.hot.accept()
